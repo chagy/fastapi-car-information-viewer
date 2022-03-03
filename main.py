640d1f5c9e522b3601255fd1748407c5bd1059aa
chagy <chagy_x_@hotmail.com>
2022-03-03 13:59:42 +0700

    19

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI,Query
+from pydantic import BaseModel,Field
+from typing import Optional,List,Dict
+from database import cars
+
+class Car(BaseModel): 
+    make: str 
+    model: str 
+    year: int = Field(...,ge=1970,lt=2022)
+    price: float 
+    engine: Optional[str] = "V4"
+    autonomous: bool 
+    sold: List[str]
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"Welcome to":"your first API in FastAPI 2"}
+
+@app.get("/cars",response_model=List[Dict[str,Car]])
+def get_cars(number: Optional[str] = Query("10",max_length=3)):
+    response=[]
+    for id,car in list(cars.items())[:int(number)]:
+        to_add = {}
+        to_add[id] = car 
+        response.append(to_add)
+    return response
